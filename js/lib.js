0bdce646ff87d7e428814d69d9f9809498a9f9fc
dat <ddat09843@example.com>
2026-05-14 08:27:16 +0700

    create my project strycture

---
+++ js/lib.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+     <head>
+           <title>Thiết kế Web</title>
+           <meta charset="UTF-8>
+     </head>
+     <body>
+        <h3>Đây là trang chủ</h3>  
+        <ul>
+          <li>
+             <a></a>
+          </li>
+        <ul>
+     </body>
+</html>
